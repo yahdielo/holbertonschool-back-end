e91d5c835189031e60e09748d95801c61039a16e
yahdielo <yahdielaz@gmail.com>
2023-04-10 17:04:26 -0400

    write to csv

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" making api requests to get specific data"""
+
+import csv
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    user_id = eval(argv[1])
+
+    user_url = "https://jsonplaceholder.typicode.com/users"
+    todo_url = "https://jsonplaceholder.typicode.com/todos"
+
+    response = requests.get(user_url)
+    _response = requests.get(todo_url)
+    todo_response = _response.json()
+    user_response = response.json()
+    task_completed = []
+    row = []
+
+    for user in user_response:
+        if user['id'] == user_id:
+            user_name = user['name']
+
+    for task in todo_response:
+        if task['userId'] == user_id:
+            rows = [user_id, user_name, task['completed'], task['title']]
+            row.append(rows)
+
+    with open('USER_ID.csv', 'w') as f:
+
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        writer.writerows(row)
