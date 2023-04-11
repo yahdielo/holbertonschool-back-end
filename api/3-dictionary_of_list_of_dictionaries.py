f079019b39875ae72eb55ce10633a167e6a11e5a
yahdielo <yahdielaz@gmail.com>
2023-04-10 20:59:48 -0400

    last task

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" making api requests to get specific data"""
+
+import json
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+
+    user_url = "https://jsonplaceholder.typicode.com/users"
+    todo_url = "https://jsonplaceholder.typicode.com/todos"
+
+    response = requests.get(user_url)
+    _response = requests.get(todo_url)
+    todo_response = _response.json()
+    user_response = response.json()
+    d_task = {}
+    full_dict = {}
+
+    for user in user_response:
+        list_of_task = []
+        user_id = user['id']
+        user_name = user['username']
+        for task in todo_response:
+            d_task = {'task': task['title'],
+                        'completed': task['completed'],
+                        'username': user_name}
+            list_of_task.append(d_task)
+        full_dict[user_id] = list_of_task
+
+
+    with open('todo_all_employees.json', 'w') as f:
+
+        json.dump(full_dict, f)
